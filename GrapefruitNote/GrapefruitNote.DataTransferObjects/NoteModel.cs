e70c0cf378e03da8700990a5b9df2aebfb749360
denis <stinger907@yahoo.com>
2014-09-18 17:03:37 +0300

    Implemented Notes controller.

--- GrapefruitNote/GrapefruitNote.DataTransferObjects/NoteModel.cs
+++ GrapefruitNote/GrapefruitNote.DataTransferObjects/NoteModel.cs
@@ -10,6 +10,8 @@
             this.Categories = new List<CategoryModel>();
         }
 
+        public int NoteId { get; set; }
+
         public string Title { get; set; }
 
         public string Text { get; set; }
